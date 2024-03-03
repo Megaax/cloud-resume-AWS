3415812635e87f21765f3fc5fcb3c5f727b74fed
Megaax <mohamedxmagdi@outlook.com>
2024-03-03 13:48:35 +0200

    Added Iac

--- infra/provider.tf
+++ infra/provider.tf
@@ -10,7 +10,7 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
 
-  access_key = "AKIAW3MEAPRWVP6Y2KA3"
-  secret_key = "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"
-  region     = "us-east-1"
+  # access_key = "AKIAW3MEAPRWVP6Y2KA3"
+  # secret_key = "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"
+  region = "us-east-1"
 }
